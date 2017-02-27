0b5d945a7f30ac76923c7630252a0c7f23acdf65
Simon Dann <simon.dann@halesway.co.uk>
2017-02-27 11:27:09 +0000

    :sparkles: updated install page so use new workspace wizard

--- app/src/renderer/components/InstallPageView.vue
+++ app/src/renderer/components/InstallPageView.vue
@@ -6,21 +6,9 @@
                     <div class="p3 px4 center">
                         <img src="./LandingPageView/assets/anvil.png" alt="anvil" style="width:64px;">
                         <h3 class="p0 pb2 m0">Welcome to Anvil!</h3>
-                        <p>Before we begin please tell me what your first project is called:</p>
-                    </div>
-                    <div class="px4">
-                        <ui-textbox
-                                error="The project name may not be more than 65 characters"
-                                help="Pick a project name not more than 65 characters"
-                                label="Project Name"
-                                placeholder="Enter a project name"
-                                :maxlength="65"
-                                :invalid="projectName.length > 65"
-                                v-model="projectName"
-                        ></ui-textbox>
                     </div>
                     <div class="px4 pb3 center">
-                        <ui-button v-on:click="saveProject" color="primary" raised size="large" :disabled="!canContinue">Add Project</ui-button>
+                        <ui-button v-on:click="continueInstalling" color="primary" raised size="large">Lets Begin</ui-button>
                     </div>
                 </div>
             </div>
@@ -29,42 +17,16 @@
 </template>
 
 <script type="text/babel">
-    import UiTextbox from 'keen-ui/lib/UiTextbox'
     import UiButton from 'keen-ui/lib/UiButton'
     export default {
         name: 'install-page',
-        data () {
-            return {
-                projectName: '',
-                isSaving: false
-            }
-        },
-        computed: {
-            canContinue () {
-                return this.projectName.length > 0 && this.projectName.length <= 65 && !this.isSaving
-            }
+        components: {
+            UiButton
         },
         methods: {
-            saveProject () {
-                let _vm = this
-                this.isSaving = true
-                this.$store.dispatch('addWorkspace', {
-                    name: this.projectName,
-                    setDefault: true
-                }).then(() => {
-                    _vm.$router.replace('dashboard-page')
-                })
+            continueInstalling () {
+                this.$store.dispatch('toggleWorkspaceCreatorVisibility')
             }
-        },
-        components: {
-            UiTextbox,
-            UiButton
         }
     }
 </script>
-
-<style rel="stylesheet/scss" lang="scss">
-    .card{
-        border: 1px solid #c7c7c7;
-    }
-</style>
